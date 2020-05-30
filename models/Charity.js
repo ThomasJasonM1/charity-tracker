f33e04b0704529a4acbb3e9839fa56f8f374337d
ThomasJasonM1 <jason@call-em-all.com>
2020-05-30 11:26:16 -0500

    Adds Models

---
+++ models/Charity.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const charitySchema = new mongoose.Schema({
+  ein: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isDonationPartner: {
+    type: Boolean,
+    default: false,
+  },
+  isVolunteerPartner: {
+    type: Boolean,
+    default: false,
+  },
+  contact: {
+    firstName: {
+      type: String,
+      default: '',
+    },
+    lastName: {
+      type: String,
+      default: '',
+    },
+    email: {
+      type: String,
+      default: '',
+    },
+    phone: {
+      type: String,
+      default: '',
+    },
+  },
+  howWeCanHelp: {
+    type: String,
+    default: '',
+  },
+  events: {
+    type: Array,
+    default: [],
+  },
+});
+
+const Charity = mongoose.model("Charity", charitySchema);
+
+module.exports = Charity;
