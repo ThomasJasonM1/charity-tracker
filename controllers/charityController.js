50fc0a211857877e0d3c0c7d0171c58c09fc2f24
ThomasJasonM1 <jason@call-em-all.com>
2020-05-30 13:12:11 -0500

    Adds controller and server files

---
+++ controllers/charityController.js
@@ -0,0 +1,36 @@
+const Charity = require("../models/Charity");
+
+module.exports = {
+  findAll: (req, res) => {
+    Charity
+      .find(req.query)
+      .sort({ ein: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: (req, res) => {
+    Charity
+      .findOne({ ein: req.params.id })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: (req, res) => {
+    Charity
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: (req, res) => {
+    Charity
+      .findOneAndUpdate({ ein: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: (req, res) => {
+    Charity
+      .findOne({ ein: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
