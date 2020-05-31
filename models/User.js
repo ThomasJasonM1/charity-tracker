b58c3fa1b3572fa94da07025486b27a6fd48ef63
nick wuerz <nawuerz@gmail.com>
2020-05-31 17:32:41 -0500

    updated user controller/model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose"),
-      Schema = mongoose.Schema,
-      bcrypt = require(bcrypt);
-      SALT_WORK_FACTOR = 10;
+      Schema = mongoose.Schema;
+      // bcrypt = require(bcrypt);
+      // SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema({
   firstName: {
@@ -35,25 +35,25 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.pre('save', ()=> {
-  const user = this;
-  //only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
-   // generate a salt
-   bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-     if (err) return next(err);
+// userSchema.pre('save', ()=> {
+//   const user = this;
+//   //only hash the password if it has been modified (or is new)
+//   if (!user.isModified('password')) return next();
+//    // generate a salt
+//    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+//      if (err) return next(err);
 
-     // override the cleartext password with the hashed one
-     user.password = hash;
-     next();
-   });
-});
+//      // override the cleartext password with the hashed one
+//      user.password = hash;
+//      next();
+//    });
+// });
 
-userSchema.methods.comparePassword = (candidatePassword, cb) => {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-}
+// userSchema.methods.comparePassword = (candidatePassword, cb) => {
+//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+//     if (err) return cb(err);
+//     cb(null, isMatch);
+//   });
+// }
 
-module.exports = mongoose.model(User, userSchema);
+module.exports = mongoose.model("User", userSchema);
