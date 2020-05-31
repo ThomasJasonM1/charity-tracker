2ee7c4f992fd845069ce959a73920364d9f908c2
nick wuerz <nawuerz@gmail.com>
2020-05-31 17:40:19 -0500

    updated user model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose"),
       Schema = mongoose.Schema;
-      // bcrypt = require(bcrypt);
-      // SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema({
   firstName: {
@@ -34,26 +32,5 @@ const userSchema = new Schema({
     required: true 
   },
 });
-
-// userSchema.pre('save', ()=> {
-//   const user = this;
-//   //only hash the password if it has been modified (or is new)
-//   if (!user.isModified('password')) return next();
-//    // generate a salt
-//    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-//      if (err) return next(err);
-
-//      // override the cleartext password with the hashed one
-//      user.password = hash;
-//      next();
-//    });
-// });
-
-// userSchema.methods.comparePassword = (candidatePassword, cb) => {
-//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-//     if (err) return cb(err);
-//     cb(null, isMatch);
-//   });
-// }
 
 module.exports = mongoose.model("User", userSchema);
